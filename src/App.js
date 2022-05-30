1648d97e1a1fe8a974b81d8060f744b7cadc431e
fayaz patel <fayaz.patel@arctechinfo.com>
2022-05-31 01:00:39 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import Create from './components/create';
+import Read from './components/read';
+import Update from './components/update';
+import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Router>
+      <nav>
+              <ul>
+                <li>
+                  <Link to="/create">Create</Link>
+                </li>
+                <li>
+                  <Link to="/read">Read</Link>
+                </li>
+              </ul>
+              </nav>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="main">
+          React Crud Operations
+          <br></br>
+          <div>
+        
+             
+              <Routes>
+                <Route path='/create' element={<Create />}  />
+                <Route path='/read/*' element={<Read/>} />
+                <Route path='/update' element={<Update/>} />
+              </Routes>
+          </div>
+        </div>
+            </Router>
       </header>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/create.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import { Button, Checkbox, Form } from 'semantic-ui-react';
+
+export default function Create() {
+    const id = Math.floor(Math.random(1234567890) + 1);
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [checkbox, setCheckbox] = useState(false);
+    const postData = () => {
+        axios.post(`http://localhost:5000/users`, {
+            id,
+            firstName,
+            lastName,
+            checkbox
+        })
+    }
+    return (
+        <div>
+            <Form className="create-form">
+                <Form.Field>
+                    <label>First Name</label>
+                    <input placeholder='First Name' onChange={(e) => setFirstName(e.target.value)}/>
+                </Form.Field>
+                <Form.Field>
+                    <label>Last Name</label>
+                    <input placeholder='Last Name' onChange={(e) => setLastName(e.target.value)}/>
+                </Form.Field>
+                <Form.Field>
+                    <Checkbox label='I agree to the Terms and Conditions' onChange={(e) => setCheckbox(!checkbox)}/>
+                </Form.Field>
+                <Button onClick={postData} type='submit'>Submit</Button>
+            </Form>
+        </div>
+    )
+}
